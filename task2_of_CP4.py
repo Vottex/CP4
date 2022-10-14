29440c1ffc1c627865c44f8c9b8f0c49c70135a9
Dima <exhibitionist@logicstreak.com>
2022-10-14 23:34:23 +0300

    add files

--- task2_of_CP4.py
+++ task2_of_CP4.py
@@ -1,8 +1,8 @@
-n, m = map(int, input("Введите размеры первого и второго массивов: ").split())
-ans = []
-a = [int(i) for i in input("Введите значения первого массива: ").split()]
-b = [int(i) for i in input("Введите значения второго массива: ").split()]
-for i in a:
-    if i in b and i not in ans:
-        ans.append(i)
-print("Общие элементы этих двух массивов: ", ans)
+a, b = map(int, input("Введите размеры первого и второго массивов: ").split())
+com_elements = []
+n = [int(i) for i in input("Введите значения первого массива: ").split()]
+m = [int(i) for i in input("Введите значения второго массива: ").split()]
+for i in n:
+    if i in m and i not in com_elements:
+        com_elements.append(i)
+print("Общие элементы этих двух массивов: ", com_elements)
